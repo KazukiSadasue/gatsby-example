e863a29321a8ef6edb401fce5e2af5e4e4558147
KazukiSadasue <kazuki.fx0915@gmail.com>
2021-01-06 00:16:26 +0900

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,48 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
-
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 module.exports = {
+  siteMetadata: {
+    title: "Gatsby Tutorial",
+    description: "some random description",
+    author: "@KazukiSadasue",
+    data: ["item 1", "item2"],
+    personal: { name: "kazuki", age: 26 },
+  },
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `v3xft1lfh8ke`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.ACCESS_TOKEN,
+      },
+    },
+  ],
 }
---
+++ src/examples/Header.js
@@ -0,0 +1,37 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+const getData = graphql`
+  query FirstQuery {
+    site {
+      info: siteMetadata {
+        personal {
+          age
+          name
+        }
+        author
+        data
+        description
+        title
+      }
+    }
+  }
+`
+const Header = () => {
+  const {
+    site: {
+      info: {
+        title,
+        personal: { name },
+      },
+    },
+  } = useStaticQuery(getData)
+
+  return (
+    <div>
+      <h1>title : {title}</h1>
+      <h1>name : {name}</h1>
+    </div>
+  )
+}
+
+export default Header
